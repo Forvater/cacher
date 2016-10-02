ef268162f3ca864e5bf2355bfeeff23df32d7631
Forvater <justmail12345@gmail.com>
2016-10-02 20:36:53 +0300

    tester fix

--- tester.py
+++ tester.py
@@ -1,6 +1,12 @@
 import requests
 
-r = requests.get('http://localhost:8000/from_cache?key=123', timeout=1)
-
-print r.text
-
+response = ''
+try :
+    r = requests.get('http://localhost:8000/from_cache?key=123', timeout=1)
+    response = r.text
+except requests.exceptions.Timeout :
+    print 'Connection timeout'
+except requests.exceptions.ConnectionError:
+    print 'Connection error'
+else :
+    print response
